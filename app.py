6ac41ba45e9595f6992984c68fc6b741255b2dd9
alex <alexandrumateiionut@gmail.com>
2024-03-16 19:46:41 +0100

    removing unnecesarry comments

--- app.py
+++ app.py
@@ -7,12 +7,10 @@ from components.generar_graficos import generar_graficos
 from components.administrar_componentes import administrar_componentes
 from components.administrar_trabajadores import administrar_trabajadores
 
-# Conexión y configuración de la base de datos
 database = "soldering_db.sqlite"
 conn = create_connection(database)
 setup_database(conn)
 
-# Definición de las opciones del menú y sus respectivas funciones
 menu_options = {
     "Gestion Tiempos Soldadura": gestion_tiempos_soldadura,
     "Gestionar Entradas": gestionar_entradas_tiempos,
@@ -21,14 +19,11 @@ menu_options = {
     "Administrar Trabajadores": administrar_trabajadores,
 }
 
-# Asignación de iconos a las opciones del menú (ajusta los iconos según corresponda)
 menu_icons = ["tools", "clipboard-data", "graph-up", "puzzle", "person"]
 
-# Creación del menú de opciones usando streamlit-option-menu
 with st.sidebar:
     selected_option = option_menu("Menú Principal", list(menu_options.keys()), 
                                   icons=menu_icons, menu_icon="cast", default_index=0)
 
-# Ejecución de la función asociada a la opción seleccionada
 if selected_option:
     menu_options[selected_option](conn)
