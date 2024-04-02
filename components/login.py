e288852c5a638096d4aae39aac32379dc5009f9b
alex <alexandrumateiionut@gmail.com>
2024-04-02 20:37:15 +0200

    translating the login page

--- components/login.py
+++ components/login.py
@@ -2,13 +2,13 @@ import streamlit as st
 from auth.auth import authenticate_user
 
 def login_page():
-    st.title("Login Page")
+    st.title("Iniciar Sesión")
     with st.form("login_form"):
-        worker_code = st.text_input("Worker Code")
-        password = st.text_input("Password", type="password")
-        login_button = st.form_submit_button("Login")
+        worker_code = st.text_input("Código de Trabajador", max_chars=10, help="Introduce tu código de trabajador")
+        password = st.text_input("Contraseña", type="password", help="Introduce tu contraseña")
+        login_button = st.form_submit_button("Iniciar Sesión")
         if login_button:
             if authenticate_user(worker_code, password):
                 st.session_state['authenticated'] = True
             else:
-                st.error("Invalid credentials")
+                st.error("Código de trabajador o contraseña incorrectos")
